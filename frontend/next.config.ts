edc25cbbcf91a030e32bcef9b70a7132d087ca9e
MedGm <elgorrim.mohamed@etu.uae.ac.ma>
2026-03-24 01:44:22 +0100

    fix API rewrite rule

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/proxy/:path*',
+        destination: 'http://64.23.157.35:8000/api/:path*', // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
