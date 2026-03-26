ade60f19339a309b05d9f620dd3d27f7772e4839
Ayoub Ait Said <ayoub.ait-said@inetum.com>
2026-03-26 02:21:50 +0100

    updating next.config.ts for prod env

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -7,12 +7,12 @@ const nextConfig: NextConfig = {
       {
         // Catch paths WITH trailing slash and strip it
         source: '/api/proxy/:path*/',
-        destination: 'http://backend:5000/api/:path*',
+        destination: 'http://64.23.157.35:8000/api/:path*',
       },
       {
         // Catch paths WITHOUT trailing slash
         source: '/api/proxy/:path*',
-        destination: 'http://backend:5000/api/:path*',
+        destination: 'http://64.23.157.35:8000/api/:path*',
       },
     ];
   },
